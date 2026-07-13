56b2c3a2cc4a8318cda0435e91f2cd6c8d1e90fe
kbrattli <kbrattli@live.no>
2026-07-13 02:49:42 +0200

    remove regular

--- reactNavigationJsStack/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['<rootDir>/jest/setup.ts'],
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|react-native-svg))',
-  ],
-};
--- reactNavigationJsStack/jest/setup.ts
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler/jestSetup';
-
-import { jest } from '@jest/globals';
-
-jest.mock('react-native-safe-area-context', () =>
-  jest.requireActual<{ default: unknown }>('react-native-safe-area-context/jest/mock').default,
-);
